451e97ec4e2d0b7a80c188e091f7246093544ea5
lumgashi <lumgashi97@hotmail.com>
2024-03-09 19:53:16 +0100

    minor fix

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -6,12 +6,19 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { Roles } from 'src/auth/decorators';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guards';
+import { UserRole } from 'types';
 
 @Controller('categories')
+@ApiTags('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
@@ -40,6 +47,8 @@ export class CategoriesController {
    * @route       PATCH /api/categories/:id.
    * @access      Private [Admin]
    */
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
   @Patch(':id')
   update(
     @Param('id') id: string,
